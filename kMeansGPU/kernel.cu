a5ada2a22a627ead9fc42dcf802458d84b81e84b
DarkDev <21362600+DarkDeyus@users.noreply.github.com>
2019-01-24 01:00:19 +0100

    Final version

--- kMeansGPU/kernel.cu
+++ kMeansGPU/kernel.cu
@@ -1,51 +1,57 @@
-#include "kernel.cuh"
-#include "string.h"
 #include <thrust/device_vector.h>
 #include <thrust/reduce.h>
 #include <iostream>
-#define THREADS_IN_BLOCK 512
-/*#define BLOCKS 1
-#define THREADS (THREADS_IN_BLOCK*BLOCKS)*/
-#define POINTS_PER_THREAD 128
-
+#include "kernel.cuh"
+#include "string.h"
 using namespace std;
 
+const int THREADS_IN_BLOCK = 512;
+
+const int POINTS_PER_THREAD = 128;
+
+
+//using kernels to locate and free 2 dimensional arrays
+
 template<typename T>
-__global__ void allocArray(T** d_array, unsigned int size1, unsigned int size2)
+__global__ void freeArray(T** d_array, unsigned int first_size)
 {
 	unsigned int tid = threadIdx.x + blockIdx.x*blockDim.x;
-	if (tid < size1)
-		d_array[tid] = (T*)malloc(size2 * sizeof(T));
+	if (tid < first_size)
+		free(d_array[tid]);
+}
+
+//result is d_array[first_size][second_size]
+template<typename T>
+__global__ void allocArray(T** d_array, unsigned int first_size, unsigned int second_size)
+{
+	unsigned int tid = threadIdx.x + blockIdx.x*blockDim.x;
+	if (tid < first_size)
+		d_array[tid] = (T*)malloc(second_size * sizeof(T));
 }
 
 template<typename T>
 static T** init_d_array(unsigned int size1, unsigned int size2)
 {
-	T** res;
-	CHECK_ERRORS(cudaMalloc(&res, size1 * sizeof(T*)));
-	allocArray << <size1 / THREADS_IN_BLOCK + !!(size1%THREADS_IN_BLOCK), THREADS_IN_BLOCK >> > (res, size1, size2);
+	T** result;
+	CHECK_ERRORS(cudaMalloc(&result, size1 * sizeof(T*)));
+	//count how many blocks do we need, add one to it if something left
+	allocArray << <first_size / THREADS_IN_BLOCK + !!(first_size % THREADS_IN_BLOCK), THREADS_IN_BLOCK >> > (result, first_size, second_size);
 	cudaDeviceSynchronize();
-	return res;
+	return result;
 }
 
-template<typename T>
-__global__ void freeArray(T** d_array, unsigned int size1)
-{
-	unsigned int tid = threadIdx.x + blockIdx.x*blockDim.x;
-	if (tid < size1)
-		free(d_array[tid]);
-}
+
 
 template<typename T>
 static void free_d_array(T** d_array, unsigned int size1)
 {
-	freeArray << <size1 / THREADS_IN_BLOCK + !!(size1%THREADS_IN_BLOCK), THREADS_IN_BLOCK >> > (d_array, size1);
+	freeArray << <first_size / THREADS_IN_BLOCK + !!(first_size%THREADS_IN_BLOCK), THREADS_IN_BLOCK >> > (d_array, first_size);
 	cudaDeviceSynchronize();
 	CHECK_ERRORS(cudaFree(d_array));
 }
 
-
-__global__ void kMeansThread(double *points, unsigned int number_of_centroids, unsigned int nT, unsigned int *membership, double * d_centres,
+// Kernel used to run threads FULL of points , every thread checks full POINTS_PER_THREAD points.
+__global__ void KMeansFullThreads(double *points, unsigned int number_of_centroids, unsigned int nT, unsigned int *membership, double * d_centres,
 	unsigned int **centroid_size, double **new_centres, unsigned int *d, unsigned int offset = 0)
 {
 	extern __shared__ double centres[];
@@ -57,11 +63,12 @@ __global__ void kMeansThread(double *points, unsigned int number_of_centroids, u
 		centroid_size[i][tid] = 0;
 	}
 
-	unsigned int tid_s = threadIdx.x;
-	while (tid_s < number_of_centroids * DIMENSION)
+	unsigned int tid_second = threadIdx.x;
+	//in case there are less threads than fields in the shared centres array
+	while (tid_second < number_of_centroids * DIMENSION)
 	{
-		centres[tid_s] = d_centres[tid_s];
-		tid_s += blockDim.x;
+		centres[tid_second] = d_centres[tid_second];
+		tid_second += blockDim.x;
 	}
 
 	for (int i = 0; i < number_of_centroids*DIMENSION; ++i)
@@ -71,96 +78,85 @@ __global__ void kMeansThread(double *points, unsigned int number_of_centroids, u
 	for (int i = 0; i < nT; ++i)
 	{
 
-		double dis = distance(&points[i*DIMENSION + nT * tid*DIMENSION], centres);
-		//printf("%d %d %f\n", tid, __LINE__, dis);
-		unsigned int c = 0;
+		double dist1 = distance(&points[i * DIMENSION + nT * tid * DIMENSION], centres);
+
+		unsigned int current_centroid_number = 0;
 		for (int j = 1; j < number_of_centroids; ++j)
 		{
-			double dis2 = distance(&points[i*DIMENSION + nT * tid*DIMENSION], &centres[j*DIMENSION]);
-			//printf("%d %d %f %f\n", tid, __LINE__, dis, dis2);
-			if (dis2 < dis)
+			double dist2 = distance(&points[i * DIMENSION + nT * tid * DIMENSION], &centres[j * DIMENSION]);
+
+			if (dist2 < dist1)
 			{
-				dis = dis2;
-				c = j;
+				dist1 = dist2;
+				current_centroid_number = j;
 			}
 		}
-		//printf("Thread %d, i = %d, dis = %f, c = %d, mem = %d\n", tid, i, dis, c, membership[i+nT*tid]);
-		if (membership[i + nT * tid] != c)
+
+		if (membership[i + nT * tid] != current_centroid_number)
 		{
 			++(d[tid]);
-			//printf("Point %d changed from %d to %d\n", i + nT * tid, membership[i + nT * tid], c);
-			membership[i + nT * tid] = c;
+			membership[i + nT * tid] = current_centroid_number;
 		}
-		centroid_size[c][tid]++;
-		//addTogether(new_centres[tid] + c * DIMENSION, &points[i*DIMENSION + nT * tid*DIMENSION]);
+		centroid_size[current_centroid_number][tid]++;
+
 		for (int j = 0; j < DIMENSION; ++j)
 		{
-			new_centres[c*DIMENSION + j][tid] += points[i*DIMENSION + nT * tid * DIMENSION + j];
+			new_centres[current_centroid_number * DIMENSION + j][tid] += points[i * DIMENSION + nT * tid * DIMENSION + j];
 		}
-	}
-	/*printf("tid %d: d = %d\n", tid, d[tid]);
-	unsigned int sum = 0;
-	for(int i = 0; i < number_of_centroids; ++i)
-	{
-		printf("tid %u, nc: (%f, %f, %f), size %u\n", tid, new_centres[i*DIMENSION][tid], new_centres[i*DIMENSION + 1][tid], new_centres[i*DIMENSION + 2][tid], centroid_size[i][tid]);
-		sum += centroid_size[i][tid];
-	}
-	if(sum != POINTS_PER_THREAD)
-	{
-		printf("sum = %u\n", sum);
-	}*/
+	}	
 }
 
-__global__ void kMeansSingleThread(double *points, unsigned int k, unsigned int nT, unsigned int *membership, double * d_centres,
-	unsigned int **csize, double **new_centres, unsigned int *d, unsigned int offset, unsigned int how_many_points)
+//kernel used with only one thread that checks how_many_points , not full POINTS_PER_THREAD
+__global__ void KMeansSingleNotFullThread(double *points, unsigned int number_of_centroids, unsigned int nT, unsigned int *membership, double * d_centres,
+	unsigned int **centroid_size, double **new_centres, unsigned int *number_of_changes, unsigned int offset, unsigned int how_many_points)
 {
 	extern __shared__ double centres[];
 	unsigned int tid = threadIdx.x + blockIdx.x * blockDim.x + offset;
-	//printf("!!!!!!!!!tid %d: d = %d i = %d\n", tid, d[tid], 0);
-	d[tid] = 0;
-	for (int i = 0; i < k; ++i)
+
+	number_of_changes[tid] = 0;
+
+	for (int i = 0; i < number_of_centroids; ++i)
 	{
-		csize[i][tid] = 0;
+		centroid_size[i][tid] = 0;
 	}
 
-	unsigned int tid_s = threadIdx.x;
-	while (tid_s < k * DIMENSION)
+	unsigned int tid_second = threadIdx.x;
+	//in case there are less threads than fields in the shared centres array
+	while (tid_second < number_of_centroids * DIMENSION)
 	{
-		centres[tid_s] = d_centres[tid_s];
-		tid_s += blockDim.x;
+		centres[tid_second] = d_centres[tid_second];
+		tid_second += blockDim.x;
 	}
 
-	for (int i = 0; i < k*DIMENSION; ++i)
+	for (int i = 0; i < number_of_centroids * DIMENSION; ++i)
 	{
 		new_centres[i][tid] = 0;
 	}
 	for (int i = 0; i < how_many_points; ++i)
 	{
-		double dis = distance(&points[i*DIMENSION + nT * tid*DIMENSION], centres);
-		//printf("%d %d %f\n", tid, __LINE__, dis);
-		unsigned int c = 0;
-		for (int j = 1; j < k; ++j)
+		double dist = distance(&points[i * DIMENSION + nT * tid * DIMENSION], centres);
+
+		unsigned int current_centroid_number = 0;
+		for (int j = 1; j < number_of_centroids; ++j)
 		{
-			double dis2 = distance(&points[i*DIMENSION + nT * tid*DIMENSION], &centres[j*DIMENSION]);
-			//printf("%d %d %f %f\n", tid, __LINE__, dis, dis2);
-			if (dis2 < dis)
+			double dist2 = distance(&points[i * DIMENSION + nT * tid * DIMENSION], &centres[j * DIMENSION]);
+
+			if (dist2 < dist)
 			{
-				dis = dis2;
-				c = j;
+				dist = dist2;
+				current_centroid_number = j;
 			}
 		}
-		//printf("Thread %d, i = %d, dis = %f, c = %d, mem = %d\n", tid, i, dis, c, membership[i+nT*tid]);
-		if (membership[i + nT * tid] != c)
+
+		if (membership[i + nT * tid] != current_centroid_number)
 		{
-			d[tid] += 1;
-			//printf("Point %d changed from %d to %d\n", i + nT * tid, membership[i + nT * tid], c);
-			membership[i + nT * tid] = c;
+			number_of_changes[tid] += 1;
+			membership[i + nT * tid] = current_centroid_number;
 		}
-		csize[c][tid]++;
-		//addTogether(new_centres[tid] + c * DIMENSION, &points[i*DIMENSION + nT * tid*DIMENSION]);
+		centroid_size[current_centroid_number][tid]++;
 		for (int j = 0; j < DIMENSION; ++j)
 		{
-			new_centres[c*DIMENSION + j][tid] += points[i*DIMENSION + nT * tid * DIMENSION + j];
+			new_centres[current_centroid_number * DIMENSION + j][tid] += points[i * DIMENSION + nT * tid * DIMENSION + j];
 		}
 	}
 }
@@ -215,7 +211,7 @@ void kMeansGPU(double points[][DIMENSION], unsigned int number_of_centroids, uns
 		CHECK_ERRORS(cudaMemcpy(d_centres, centres, number_of_centroids * DIMENSION * sizeof(double), cudaMemcpyHostToDevice));
 		if (BLOCKS)
 		{			
-			kMeansThread <<< BLOCKS, THREADS_IN_BLOCK, number_of_centroids * DIMENSION * sizeof(double) >> > (d_points, number_of_centroids, POINTS_PER_THREAD,
+			KMeansFullThreads <<< BLOCKS, THREADS_IN_BLOCK, number_of_centroids * DIMENSION * sizeof(double) >> > (d_points, number_of_centroids, POINTS_PER_THREAD,
 					d_membership, d_centres, d_centroid_size, d_new_centres, d_number_of_changes, 0);
 			CHECK_ERRORS(cudaDeviceSynchronize());
 		}
@@ -223,15 +219,15 @@ void kMeansGPU(double points[][DIMENSION], unsigned int number_of_centroids, uns
 
 		if (THREADS_IN_NOT_FULL_BLOCK)
 		{			
-			kMeansThread << < 1, THREADS_IN_NOT_FULL_BLOCK, number_of_centroids * DIMENSION * sizeof(double) >> > (d_points, number_of_centroids, POINTS_PER_THREAD,
+			KMeansFullThreads << < 1, THREADS_IN_NOT_FULL_BLOCK, number_of_centroids * DIMENSION * sizeof(double) >> > (d_points, number_of_centroids, POINTS_PER_THREAD,
 					d_membership, d_centres, d_centroid_size, d_new_centres, d_number_of_changes, THREADS_IN_BLOCK * BLOCKS);
 			CHECK_ERRORS(cudaDeviceSynchronize());			
 		}
 
 		if (POINTS_IN_NOT_FULL_THREAD)
 		{
-			kMeansSingleThread <<< 1, 1, number_of_centroids*DIMENSION * sizeof(double) >> > (d_points, number_of_centroids, POINTS_PER_THREAD, d_membership, d_centres, d_centroid_size,
-				d_new_centres, d_number_of_changes, THREADS_IN_BLOCK*BLOCKS + THREADS_IN_NOT_FULL_BLOCK, POINTS_IN_NOT_FULL_THREAD);
+			KMeansSingleNotFullThread <<< 1, 1, number_of_centroids*DIMENSION * sizeof(double) >> > (d_points, number_of_centroids, POINTS_PER_THREAD, d_membership, d_centres, d_centroid_size,
+				d_new_centres, d_number_of_changes, THREADS_IN_BLOCK *BLOCKS + THREADS_IN_NOT_FULL_BLOCK, POINTS_IN_NOT_FULL_THREAD);
 			CHECK_ERRORS(cudaDeviceSynchronize());
 		}
 
@@ -240,27 +236,26 @@ void kMeansGPU(double points[][DIMENSION], unsigned int number_of_centroids, uns
 		{
 			thrust::device_ptr<unsigned int> d_1 = thrust::device_pointer_cast(d_centroid_size_h[i]);
 			unsigned int quotient = thrust::reduce(d_1, d_1 + THREADS);
-			
+
+			//if there is no point in a centroid , we would divide by 0, so we set it to 1
 			//quotient = quotient == 0 ? 1 : quotient;
-			//printf("i = %d, quotient = %u\n", i, quotient);
+			//printf("centroid number is %d, quotient is %u\n", i, quotient);
 			for (int j = 0; j < DIMENSION; ++j)
 			{
 				thrust::device_ptr<double> d_2 = thrust::device_pointer_cast(d_new_centres_h[i * DIMENSION + j]);
 				centres[i][j] = thrust::reduce(d_2, d_2 + THREADS) / quotient;
-
-				//centres[i][j] = sumArray(d_new_centres_h[i*DIMENSION + j], THREADS) / quotient;
 			}
 			//printf("center of %d is : %f, %f, %f\n", i, centres[i][0], centres[i][1], centres[i][2]);
 		}
 		thrust::device_ptr<unsigned int> d_3 = thrust::device_pointer_cast(d_number_of_changes);
 		number_of_changes = thrust::reduce(d_3, d_3 + THREADS);
 		
-		//printf("End of iteration (i = %d, d = %u)\n", iteration , d);
+		//printf("End of iteration number %d, d is equal to %u)\n", iteration , d);
 		++iteration;
 
 	} while (number_of_changes / (float)number_of_points > threshold);
 
-	printf("Iterations: %u", number_of_changes);
+	printf("Number of iterations: %u", number_of_changes);
 
 	CHECK_ERRORS(cudaMemcpy(membership, d_membership, sizeof(unsigned int)*number_of_points, cudaMemcpyDeviceToHost));
 
